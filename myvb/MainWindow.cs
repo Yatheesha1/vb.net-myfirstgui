849566cf23470adf56005f67338a9ab786d7d0f0
Yatheesha S <yatheesha.sunnara.k@gmail.com>
2017-07-12 15:52:09 +0530

    first commit

---
+++ myvb/MainWindow.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void OnEntryNameChanged (object sender, EventArgs e)
+	{
+		labelStatus.Text = entryName.Text;
+		saveAction.Sensitive = true;
+	}
+
+	protected void OnNewActionActivated (object sender, EventArgs e)
+	{
+		entryName.Text = "";
+		saveAction.Sensitive = false;
+	}
+}
